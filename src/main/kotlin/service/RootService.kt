249b5d3b3d7293c6bfe4b8ddee2e60820689de02
ChristianHering <christian.hering@tu-dortmund.de>
2023-06-07 16:49:03 +0200

    Refreshable hinzugefügt + Rootservice und AbstractRefreshingService implementiert

--- src/main/kotlin/service/RootService.kt
+++ src/main/kotlin/service/RootService.kt
@@ -2,10 +2,33 @@ package service
 
 import entity.Sagani
 
+/**
+ * [RootService] allows the view layer to communicate to service layer.
+ * @property gameService
+ * @property playerActionService
+ * @property kIService
+ * @property networkService
+ * @property currentGame
+ */
 class RootService {
     val gameService = GameService(this)
     val playerActionService = PlayerActionService(this)
     val kIService = KIService(this)
     val networkService = NetworkService(this)
-    val currentGame: Sagani? = null
+    var currentGame: Sagani? = null
+
+    /**
+     * Add [refreshable] in gameService und playerActionService
+     */
+    private fun addRefreshable(refreshable: view.Refreshable) {
+        gameService.addRefreshable(refreshable)
+        playerActionService.addRefreshable(refreshable)
+    }
+
+    /**
+     * Add multiple [refreshable] in gameService und playerActionService
+     */
+    fun addEachRefreshable(vararg refreshable: view.Refreshable) {
+        refreshable.forEach { addRefreshable(it) }
+    }
 }
