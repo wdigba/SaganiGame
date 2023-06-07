8e9b52bfc7c6a37891f13e720df6f744b1b0065f
ChristianHering <christian.hering@tu-dortmund.de>
2023-06-07 16:49:03 +0200

    Refreshable hinzugefügt + Rootservice und AbstractRefreshingService implementiert

--- src/main/kotlin/service/AbstractRefreshingService.kt
+++ src/main/kotlin/service/AbstractRefreshingService.kt
@@ -1,4 +1,23 @@
 package service
 
+import view.Refreshable
+
+/**
+ * [AbstractRefreshingService] connects service layer and view layer
+ */
 abstract class AbstractRefreshingService {
+    private val refreshables = mutableListOf<Refreshable>()
+
+    /**
+     * Add [Refreshable] to refreshable
+     */
+    fun addRefreshable(newRefreshable: Refreshable) {
+        refreshables += newRefreshable
+    }
+
+    /**
+     * refresh GUI
+     */
+    fun onAllRefreshables(method: Refreshable.() -> Unit) =
+        refreshables.forEach { it.method() }
 }
