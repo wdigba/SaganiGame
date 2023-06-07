40bf76083a1808ebcce95a78ce69463b226f8e66
ChristianHering <christian.hering@tu-dortmund.de>
2023-06-07 15:38:37 +0200

    Service-Schicht: leere Klassen

--- src/main/kotlin/service/RootService.kt
+++ src/main/kotlin/service/RootService.kt
@@ -1,8 +1,11 @@
 package service
 
+import entity.Sagani
+
 class RootService {
     val gameService = GameService(this)
     val playerActionService = PlayerActionService(this)
     val kIService = KIService(this)
     val networkService = NetworkService(this)
+    val currentGame: Sagani? = null
 }
