15421b404897890fddb01284404aad3e261692e4
ChristianHering <christian.hering@tu-dortmund.de>
2023-06-08 06:54:03 +0200

    Implementierung von Refreshable + TestRefreshable

--- src/test/kotlin/service/TestRefreshable.kt
+++ src/test/kotlin/service/TestRefreshable.kt
@@ -8,15 +8,95 @@ import view.Refreshable
 class TestRefreshable : Refreshable {
     var refreshAfterStartNewGameCalled: Boolean = false
         private set
+    var refreshAfterChangeToNextPlayerCalled: Boolean = false
+        private set
+    var refreshAfterCalculateWinnerCalled: Boolean = false
+        private set
+    var refreshAfterCalculatePointsCalled: Boolean = false
+        private set
+    var refreshAfterSaveGameCalled: Boolean = false
+        private set
+    var refreshAfterLoadGameCalled: Boolean = false
+        private set
+    var refreshAfterPlaceTileCalled: Boolean = false
+        private set
+    var refreshAfterRotateTileCalled: Boolean = false
+        private set
+    var refreshAfterrefreshUndoCalled: Boolean = false
+        private set
 
     /**
      * reset all properties
      */
     fun reset() {
         refreshAfterStartNewGameCalled = false
+        refreshAfterChangeToNextPlayerCalled = false
+        refreshAfterCalculateWinnerCalled = false
+        refreshAfterCalculatePointsCalled = false
+        refreshAfterSaveGameCalled = false
+        refreshAfterLoadGameCalled = false
+        refreshAfterPlaceTileCalled = false
+        refreshAfterRotateTileCalled = false
+        refreshAfterrefreshUndoCalled = false
     }
 
+    /**
+     * Tests can check if refreshAfterStartNewGame was called
+     */
     override fun refreshAfterStartNewGame() {
         refreshAfterStartNewGameCalled = true
+    }
+
+    /**
+     * Tests can check if refreshAfterChangeToNextPlayer was called
+     */
+    override fun refreshAfterChangeToNextPlayer() {
+        refreshAfterChangeToNextPlayerCalled = true
+    }
+
+    /**
+     * Tests can check if refreshAfterCalculateWinner was called
+     */
+    override fun refreshAfterCalculateWinner() {
+        refreshAfterCalculateWinnerCalled = true
+    }
+
+    /**
+     * Tests can check if refreshAfterCalculatePoints was called
+     */
+    override fun refreshAfterCalculatePoints() {
+        refreshAfterCalculatePointsCalled = true
+    }
+
+    /**
+     * Tests can check if refreshAfterSaveGame was called
+     */
+    override fun refreshAfterSaveGame() {
+        refreshAfterSaveGameCalled = true
+    }
+
+    /**
+     * Tests can check if refreshAfterLoadGame was called
+     */
+    override fun refreshAfterLoadGame() {
+        refreshAfterLoadGameCalled = true
+    }
+
+    /**
+     * Tests can check if refreshAfterPlaceTile was called
+     */
+    override fun refreshAfterPlaceTile() {
+        refreshAfterPlaceTileCalled = true
+    }
+
+    /**
+     * Tests can check if refreshAfterRotateTile was called
+     */
+    override fun refreshAfterRotateTile() {
+        refreshAfterRotateTileCalled = true
+    }
+
+    override fun refreshUndo() {
+        refreshAfterrefreshUndoCalled = true
     }
 }
