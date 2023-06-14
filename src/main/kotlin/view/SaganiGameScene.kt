1f647f4ca78eb79c553968d20ae832077c0f3a6a
marie.quell <marie.quell@tu-dortmund.de>
2023-06-14 10:45:34 +0200

    Rebase mit Main

--- src/main/kotlin/view/SaganiGameScene.kt
+++ src/main/kotlin/view/SaganiGameScene.kt
@@ -2,9 +2,12 @@ package view
 
 import service.RootService
 import tools.aqua.bgw.components.uicomponents.Label
+import tools.aqua.bgw.core.Alignment
 import tools.aqua.bgw.core.BoardGameScene
 import tools.aqua.bgw.util.Font
 import tools.aqua.bgw.visual.ColorVisual
+import tools.aqua.bgw.components.uicomponents.Button
+
 
 class SaganiGameScene(private val rootService: RootService) : BoardGameScene(1920, 1080), Refreshable{
     private val headlineLabel = Label(
@@ -12,9 +15,28 @@ class SaganiGameScene(private val rootService: RootService) : BoardGameScene(192
         text = "GameScene - Comming Soon",
         font = Font(size = 22)
     )
+
+    private val undoButton = Button(
+        width = 170, height = 40,
+        posX = 50, posY = 980,
+        text = "UNDO", font = Font(size = 30), isWrapText = true
+    ).apply {
+        visual = ColorVisual(196, 187, 147)
+        alignment = Alignment.TOP_CENTER
+    }
+    private val redoButton = Button(
+        width = 170, height = 40,
+        posX = 250, posY = 980,
+        text = "REDO", font = Font(size = 30), alignment = Alignment.CENTER_LEFT
+    ).apply {
+        visual = ColorVisual(196, 187, 147)
+    }
+
+
     init {
         background = ColorVisual(158, 181, 91)
-        addComponents(headlineLabel)
+        addComponents(headlineLabel,
+       undoButton,redoButton )
     }
 
 }
