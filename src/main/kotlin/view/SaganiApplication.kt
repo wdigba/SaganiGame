52c6f7dee8499c55a22f5aaadf74a92e2a2e33d4
marie.quell <marie.quell@tu-dortmund.de>
2023-06-14 11:16:34 +0200

    Rekusives Problem gelöst

--- src/main/kotlin/view/SaganiApplication.kt
+++ src/main/kotlin/view/SaganiApplication.kt
@@ -6,23 +6,27 @@ import tools.aqua.bgw.core.BoardGameApplication
 class SaganiApplication : BoardGameApplication("SoPra Game") {
     private val rootService = RootService()
 
-    private val playerConfigScene = PlayerConfigScene(rootService).apply  {
+    private val playerConfigScene: PlayerConfigScene = PlayerConfigScene(rootService).apply {
+
         //TODO: nur zu Testzwecken
         startButton.onMouseClicked = {
             this@SaganiApplication.hideMenuScene()
         }
+        backButton.onMouseClicked = {
+            this@SaganiApplication.showMenuScene(newGameMenuScene)
+        }
 
     }
 
 
-    private val configurationScene = ConfigurationScene(rootService).apply {
+    private val configurationScene: ConfigurationScene = ConfigurationScene(rootService).apply {
         playersButton.onMouseClicked = {
             this@SaganiApplication.showMenuScene(playerConfigScene)
         }
     }
-    private val saganiGameScene = SaganiGameScene(rootService)
-    private val ruleScene = RuleScene(rootService)
-    private val newGameMenuScene = NewGameMenuScene(rootService).apply {
+    private val saganiGameScene: SaganiGameScene = SaganiGameScene(rootService)
+    private val ruleScene: RuleScene = RuleScene(rootService)
+    private val newGameMenuScene: NewGameMenuScene = NewGameMenuScene(rootService).apply {
         kIButton.onMouseClicked = {
             hideMenuScene()
             this@SaganiApplication.showGameScene(saganiGameScene)
