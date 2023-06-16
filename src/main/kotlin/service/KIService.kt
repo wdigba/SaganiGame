30b3a43b0af66e22c2a9c358a69018d4adf389b6
Noah <104321423+noah-bedano@users.noreply.github.com>
2023-06-16 14:58:31 +0200

    Adding function to calculate score of the board
    TODOS:
    
    The getNewScoreMapForTile, does not check if the arrows are satisfied, so the returned Score map might be wrong if some arrows are already satisfied and therefore should not be calculated in with the open one
    
    To fix this a new placed tile could calculate first which arrows would be satisfied already and place discs on them before calling getNewScorMapForTile

--- src/main/kotlin/service/KIService.kt
+++ src/main/kotlin/service/KIService.kt
@@ -9,7 +9,7 @@ import java.util.*
  * The service responsible for executing the AI strategies
  */
 class KIService(private val rootService: RootService) {
-    private class CoordinateInformation {
+    class CoordinateInformation {
 
         // variables to count how many open arrows show on this coordinate
         var airCount = 0
@@ -28,17 +28,36 @@ class KIService(private val rootService: RootService) {
 
         // is the position occupied by a tile already
         var occupied = false
+
+        fun copy(): CoordinateInformation {
+            val copy = CoordinateInformation()
+            copy.airCount = this.airCount
+            copy.earthCount = this.earthCount
+            copy.waterCount = this.waterCount
+            copy.fireCount = this.fireCount
+            copy.discsIfAirPlaced = this.discsIfAirPlaced
+            copy.discsIfEarthPlaced = this.discsIfEarthPlaced
+            copy.discsIfWaterPlaced = this.discsIfWaterPlaced
+            copy.discsIfFirePlaced = this.discsIfFirePlaced
+            copy.gameDistance = this.gameDistance
+            copy.occupied = this.occupied
+            return copy
+        }
     }
 
     fun checkArrowAlignmentMetrix(player: Player){
-        var board = player.board
+        val scoreMap = buildScoreMap(player, 3)
+
+
 
     }
 
-    fun buildScoreMap(player: Player, range: Int) {
+
+
+    fun buildScoreMap(player: Player, range: Int) : Map<Pair<Int, Int>, CoordinateInformation>{
         val scoreMap = fillScoreMap(player, range)
         updateScoreMapForArrows(player, scoreMap)
-
+        return scoreMap
     }
 
 
@@ -53,7 +72,7 @@ class KIService(private val rootService: RootService) {
     }
 
 
-    private fun isOnlyUnsatisfiedArrow(currentArrow: Arrow, tile: Tile): Boolean {
+    fun isOnlyUnsatisfiedArrow(currentArrow: Arrow, tile: Tile): Boolean {
         var unsatisfiedCount = 0
         for (arrow in tile.arrows) {
             if (arrow.disc.size == 0) {
@@ -63,46 +82,136 @@ class KIService(private val rootService: RootService) {
         return unsatisfiedCount == 1 && currentArrow.disc.size == 0
     }
 
+    private fun deepCopyScoreMap(scoreMap: MutableMap<Pair<Int, Int>, CoordinateInformation>): MutableMap<Pair<Int, Int>, CoordinateInformation> {
+        val newScoreMap = mutableMapOf<Pair<Int, Int>, CoordinateInformation>()
+        for ((position, info) in scoreMap) {
+            newScoreMap[position] = info.copy()
+        }
+        return newScoreMap
+    }
 
 
-    private fun updateScoreMapForArrows(player: Player, scoreMap: MutableMap<Pair<Int, Int>, CoordinateInformation>) {
-        for ((position, tile) in player.board) {
-            for (arrow in tile.arrows) {
-                var adjacentPos = calculateAdjacentPosition(position, arrow.direction)
-                while (scoreMap.containsKey(adjacentPos)) {
-                    val info = scoreMap[adjacentPos]!!
-                    val isTheOnlyUnsatisfiedArrow = isOnlyUnsatisfiedArrow(arrow, tile)
-                    when (arrow.element) {
-                        AIR -> {
-                            info.airCount++
-                            if (isTheOnlyUnsatisfiedArrow) {
-                                info.discsIfAirPlaced += tile.discs.size
-                            }
-                        }
-                        EARTH -> {
-                            info.earthCount++
-                            if (isTheOnlyUnsatisfiedArrow) {
-                                info.discsIfEarthPlaced += tile.discs.size
-                            }
-                        }
-                        WATER -> {
-                            info.waterCount++
-                            if (isTheOnlyUnsatisfiedArrow) {
-                                info.discsIfWaterPlaced += tile.discs.size
-                            }
-                        }
-                        FIRE -> {
-                            info.fireCount++
-                            if (isTheOnlyUnsatisfiedArrow) {
-                                info.discsIfFirePlaced += tile.discs.size
-                            }
-                        }
-                    }
-                    adjacentPos = calculateAdjacentPosition(adjacentPos, arrow.direction)
+    fun calculatePotentialTilePlacements(tile: Tile, scoreMap: MutableMap<Pair<Int, Int>, CoordinateInformation>): Map<Pair<Int, Int>, Double> {
+        val potentialScores = mutableMapOf<Pair<Int, Int>, Double>()
+
+        for ((position, coordinateInfo) in scoreMap) {
+            if (coordinateInfo.occupied || coordinateInfo.gameDistance != 0) {
+                continue
+            }
+
+            for (rotation in Direction.values()) {
+                tile.rotate(rotation)
+                val updatedScoreMap = getNewScoreMapForTile(scoreMap, tile, position)
+                val score = calculateBoardScore(updatedScoreMap)
+                potentialScores[position] = score
+            }
+
+        }
+
+        return potentialScores
+    }
+
+    private fun updatePosition(
+        newScoreMap: MutableMap<Pair<Int, Int>, CoordinateInformation>,
+        positionToUpdate: Pair<Int, Int>,
+        arrow: Arrow,
+        tile: Tile
+    ) {
+        val info = newScoreMap[positionToUpdate]!!
+        val isTheOnlyUnsatisfiedArrow = isOnlyUnsatisfiedArrow(arrow, tile)
+        when (arrow.element) {
+            AIR -> {
+                info.airCount++
+                if (isTheOnlyUnsatisfiedArrow) {
+                    info.discsIfAirPlaced += tile.discs.size
+                }
+            }
+
+            EARTH -> {
+                info.earthCount++
+                if (isTheOnlyUnsatisfiedArrow) {
+                    info.discsIfEarthPlaced += tile.discs.size
+                }
+            }
+
+            WATER -> {
+                info.waterCount++
+                if (isTheOnlyUnsatisfiedArrow) {
+                    info.discsIfWaterPlaced += tile.discs.size
+                }
+            }
+
+            FIRE -> {
+                info.fireCount++
+                if (isTheOnlyUnsatisfiedArrow) {
+                    info.discsIfFirePlaced += tile.discs.size
                 }
             }
         }
     }
+
+    private fun getNewScoreMapForTile(scoreMap: MutableMap<Pair<Int, Int>, CoordinateInformation>, tile: Tile, position: Pair<Int, Int>): MutableMap<Pair<Int, Int>, CoordinateInformation> {
+        val newScoreMap = deepCopyScoreMap(scoreMap)
+        val tileInfo = newScoreMap[position] ?: CoordinateInformation()
+        tileInfo.occupied = true
+        newScoreMap[position] = tileInfo
+        for (arrow in tile.arrows) {
+            if (arrow.disc.size == 1) {
+                continue
+            }
+            var adjacentPos = calculateAdjacentPosition(position, arrow.direction)
+            while (newScoreMap.containsKey(adjacentPos)) {
+                updatePosition(newScoreMap, adjacentPos, arrow, tile)
+                adjacentPos = calculateAdjacentPosition(adjacentPos, arrow.direction)
+            }
+        }
+        return newScoreMap
+    }
+
+    fun calculateBoardScore(scoreMap: MutableMap<Pair<Int, Int>, CoordinateInformation>): Double {
+
+        //weights
+        val arrowDensityWeight = 1
+        val disFreedomWeight = 1
+        val interferenceWeight = 1
+
+        var totalArrowDensityScore = 0.0
+        var totalDiscFreedomScore = 0.0
+        var totalInteferenceScore = 0.0
+
+        for (info in scoreMap.values) {
+            if (!info.occupied) {
+                // Increase score based on arrow density and proximity
+                val arrowDensityScore =
+                    (info.airCount * info.airCount + info.earthCount * info.earthCount + info.waterCount * info.waterCount + info.fireCount * info.fireCount) / (info.gameDistance + 1.0)
+                totalArrowDensityScore += arrowDensityScore
+
+                // Increase score based on potential disc freedom and proximity
+                val discFreedomScore =
+                    (info.discsIfAirPlaced * info.discsIfAirPlaced + info.discsIfEarthPlaced * info.discsIfEarthPlaced + info.discsIfWaterPlaced * info.discsIfWaterPlaced + info.discsIfFirePlaced * info.discsIfFirePlaced) / (info.gameDistance + 1.0)
+                totalDiscFreedomScore += discFreedomScore
+
+                // Decrease score if placing one element interferes with the placement of others
+                val interferenceScore =
+                    ((info.airCount * info.earthCount + info.airCount * info.waterCount + info.airCount * info.fireCount
+                            + info.earthCount * info.waterCount + info.earthCount * info.fireCount
+                            + info.waterCount * info.fireCount) / (info.gameDistance + 1.0))
+                totalInteferenceScore += interferenceScore
+            }
+        }
+
+        return arrowDensityWeight * totalArrowDensityScore + disFreedomWeight * totalDiscFreedomScore + interferenceWeight * totalInteferenceScore
+    }
+
+
+
+
+    private fun updateScoreMapForArrows(player: Player, scoreMap: MutableMap<Pair<Int, Int>, CoordinateInformation>) {
+        for ((position, tile) in player.board) {
+            scoreMap.putAll(getNewScoreMapForTile(scoreMap, tile, position))
+        }
+    }
+
     private fun getLowestGameDistanceFromNeighbours(position: Pair<Int, Int>, scoreMap: Map<Pair<Int, Int>, CoordinateInformation>): Int {
         var lowestGameDistance = Int.MAX_VALUE
         for (direction in Direction.values()) {
@@ -116,7 +225,7 @@ class KIService(private val rootService: RootService) {
 
 
 
-    private fun fillScoreMap(player: Player, range: Int): MutableMap<Pair<Int, Int>, CoordinateInformation> {
+    fun fillScoreMap(player: Player, range: Int): MutableMap<Pair<Int, Int>, CoordinateInformation> {
         val scoreMap = mutableMapOf<Pair<Int, Int>, CoordinateInformation>()
         val queue: Queue<Pair<Int, Int>> = LinkedList()
 
