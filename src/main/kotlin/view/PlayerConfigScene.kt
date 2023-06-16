d483046279320d49237a377f999b393cd4ab9351
marie.quell <marie.quell@tu-dortmund.de>
2023-06-16 12:52:39 +0200

    Random Names Button Schrift weiß checkIfStartIsAvailable implementiert um Startbutton (de-)aktivieren

--- src/main/kotlin/view/PlayerConfigScene.kt
+++ src/main/kotlin/view/PlayerConfigScene.kt
@@ -42,7 +42,7 @@ class PlayerConfigScene(private val rootService: RootService) :
         posX = 70, posY = 160
     ).apply {
         onKeyTyped = {
-            startButton.isDisabled = this.text.isBlank() || player2Input.text.isBlank()
+            startButton.isDisabled = checkIfStartIsAvailable()
         }
     }
 
@@ -80,7 +80,7 @@ class PlayerConfigScene(private val rootService: RootService) :
         posX = 70, posY = 240
     ).apply {
         onKeyTyped = {
-            startButton.isDisabled = player1Input.text.isBlank() || this.text.isBlank()
+            startButton.isDisabled = checkIfStartIsAvailable()
         }
     }
 
@@ -101,7 +101,7 @@ class PlayerConfigScene(private val rootService: RootService) :
         posX = 70, posY = 320
     ).apply {
         onKeyTyped = {
-            startButton.isDisabled = this.text.isBlank() || player2Input.text.isBlank()
+            startButton.isDisabled = checkIfStartIsAvailable()
         }
     }
 
@@ -121,7 +121,7 @@ class PlayerConfigScene(private val rootService: RootService) :
         posX = 70, posY = 400
     ).apply {
         onKeyTyped = {
-            startButton.isDisabled = this.text.isBlank() || player2Input.text.isBlank()
+            startButton.isDisabled = checkIfStartIsAvailable()
         }
     }
 
@@ -141,7 +141,7 @@ class PlayerConfigScene(private val rootService: RootService) :
         text = "Go Back",
         font = Font(size = 18, color = Color.white)
     ).apply {
-        visual = ColorVisual(96,108,56)
+        visual = ColorVisual(96, 108, 56)
         onMouseClicked = {
             for (input in playerInputs) {
                 input.text = ""
@@ -159,7 +159,7 @@ class PlayerConfigScene(private val rootService: RootService) :
         text = "Start",
         font = Font(size = 18, color = Color.white)
     ).apply {
-        visual = ColorVisual(96,108,56)
+        visual = ColorVisual(96, 108, 56)
         onMouseClicked = {
             val playerNames = mutableListOf<String>()
             for (input in playerInputs) {
@@ -180,7 +180,7 @@ class PlayerConfigScene(private val rootService: RootService) :
         posX = 320, posY = 240,
         text = "+", font = Font(size = 14), alignment = Alignment.CENTER
     ).apply {
-        visual = ColorVisual(200,202,167)
+        visual = ColorVisual(200, 202, 167)
         onMouseClicked = {
             repositionButtonsPlus()
         }
@@ -191,7 +191,7 @@ class PlayerConfigScene(private val rootService: RootService) :
         posX = 360, posY = 320,
         text = "-", font = Font(size = 20), alignment = Alignment.CENTER
     ).apply {
-        visual = ColorVisual(200,202,167)
+        visual = ColorVisual(200, 202, 167)
         onMouseClicked = {
             repositionButtonsMinus()
         }
@@ -204,9 +204,9 @@ class PlayerConfigScene(private val rootService: RootService) :
     private val randomNamesButton = Button(
         width = 200, height = 35,
         posX = 50, posY = 520,
-        text = "Random Names", font = Font(size = 18), alignment = Alignment.CENTER
+        text = "Random Names", font = Font(size = 18, color = Color.WHITE), alignment = Alignment.CENTER
     ).apply {
-        visual = ColorVisual(96,108,56)
+        visual = ColorVisual(96, 108, 56)
         onMouseClicked = {
             val randomNames = mutableListOf(
                 "Till", "Marc", "Luka", "Sven", "Nick", "Friedemann", "Moritz", "Stefan", "Kai", "Vadym",
@@ -219,47 +219,48 @@ class PlayerConfigScene(private val rootService: RootService) :
             for (input in playerInputs) {
                 input.text = randomAdjectives.random() + " " + randomNames.random()
             }
+            startButton.isDisabled = checkIfStartIsAvailable()
         }
     }
 
     init {
-        background = ColorVisual(254,250,224)
+        background = ColorVisual(254, 250, 224)
         player3Label.isVisible = false
         player3Input.isVisible = false
         player4Input.isVisible = false
         player4Label.isVisible = false
         minusButton.isVisible = false
         startButton.isDisabled = true
-        color3Label.isVisible =false
-        color4Label.isVisible =false
+        color3Label.isVisible = false
+        color4Label.isVisible = false
         comboBox3.isVisible = false
         comboBox4.isVisible = false
 
         comboBox1.formatFunction = {
-                "${it.toString()}"
-            }
-        comboBox1.items = mutableListOf(0.0,0.1,0.4,0.2)
+            "${it.toString()}"
+        }
+        comboBox1.items = mutableListOf(0.0, 0.1, 0.4, 0.2)
         comboBox1.selectedItemProperty.addListener { _, newValue ->
-                outputLabel.text = "Combo box selection is : $newValue"
-            }
+            outputLabel.text = "Combo box selection is : $newValue"
+        }
         comboBox2.formatFunction = {
             "${it.toString()}"
         }
-        comboBox2.items = mutableListOf(0.0,0.1,0.4,0.2)
+        comboBox2.items = mutableListOf(0.0, 0.1, 0.4, 0.2)
         comboBox2.selectedItemProperty.addListener { _, newValue ->
             outputLabel.text = "Combo box selection is : $newValue"
         }
         comboBox3.formatFunction = {
             "${it.toString()}"
         }
-        comboBox3.items = mutableListOf(0.0,0.1,0.4,0.2)
+        comboBox3.items = mutableListOf(0.0, 0.1, 0.4, 0.2)
         comboBox3.selectedItemProperty.addListener { _, newValue ->
             outputLabel.text = "Combo box selection is : $newValue"
         }
         comboBox4.formatFunction = {
             "${it.toString()}"
         }
-        comboBox4.items = mutableListOf(0.0,0.1,0.4,0.2)
+        comboBox4.items = mutableListOf(0.0, 0.1, 0.4, 0.2)
         comboBox4.selectedItemProperty.addListener { _, newValue ->
             outputLabel.text = "Combo box selection is : $newValue"
         }
@@ -267,10 +268,10 @@ class PlayerConfigScene(private val rootService: RootService) :
         opacity = .5
         addComponents(
             headlineLabel,
-            player1Label, player1Input, comboBox1, color1Label,
+            player1Label, player1Input, color1Label, comboBox1,
             player2Label, player2Input, color2Label, comboBox2,
-            player3Label, player3Input,color3Label, comboBox3,
-            player4Label, player4Input,color4Label, comboBox4,
+            player3Label, player3Input, color3Label, comboBox3,
+            player4Label, player4Input, color4Label, comboBox4,
             startButton, backButton, plusButton, minusButton,
             randomNamesButton
         )
@@ -283,16 +284,16 @@ class PlayerConfigScene(private val rootService: RootService) :
             plusButton.reposition(320, 320)
             player3Label.isVisible = true
             player3Input.isVisible = true
-            color3Label.isVisible =true
-            comboBox3.isVisible =true
+            color3Label.isVisible = true
+            comboBox3.isVisible = true
             playerInputs.add(player3Input)
 
         } else if (!player4Label.isVisible) {
             minusButton.reposition(360, 400)
             player4Input.isVisible = true
             player4Label.isVisible = true
-            color4Label.isVisible =true
-            comboBox4.isVisible =true
+            color4Label.isVisible = true
+            comboBox4.isVisible = true
             plusButton.isVisible = false
             playerInputs.add(player4Input)
         }
@@ -315,7 +316,7 @@ class PlayerConfigScene(private val rootService: RootService) :
             player3Label.isVisible = false
             player3Input.text = ""
             player3Input.isVisible = false
-            color3Label.isVisible =false
+            color3Label.isVisible = false
             comboBox3.isVisible = false
             playerInputs.remove(player3Input)
             minusButton.isVisible = false
@@ -324,5 +325,17 @@ class PlayerConfigScene(private val rootService: RootService) :
         }
     }
 
+    /**
+     * checks if one of the player inputs are empty. If so, the startbutton stays disabled.
+     * If none of the player inputs is empty, the start button is enabled.
+     */
+    private fun checkIfStartIsAvailable(): Boolean {
+        for (input in playerInputs) {
+            if (input.text == "") {
+                return true
+            }
+        }
+        return false
+    }
 
 }
