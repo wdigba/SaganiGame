98bcfa9c6a724885a946ab16d2c1b46f083021a3
Karina <karina93209@gmail.com>
2023-06-26 02:43:06 +0200

    KI Service for random move

---
+++ src/main/kotlin/service/KIServiceRandom.kt
@@ -0,0 +1,41 @@
+package service
+
+import entity.*
+import kotlin.random.Random
+
+class KIServiceRandom (private val rootService: RootService) {
+    // check offer display and get any tile
+    // check the board for available places
+    // place the tile in the random direction
+    // give a turn to the next player
+
+    fun calculateRandomMove () {
+        val currentGame = rootService.currentGame
+        checkNotNull(currentGame) { "There is no game." }
+
+        val player = currentGame.actPlayer
+
+        // random available location for the tile
+        val randomLocation = rootService.playerActionService.validLocation(player.board).random()
+
+        // random direction for the tile
+        val randomDirection = Direction.tileDirection().random()
+
+        // random tile based on game situation
+        val randomTile = if (currentGame.intermezzo) {
+            // choosing random tile from intermezzo storage
+            currentGame.intermezzoStorage.random()
+        } else if (currentGame.offerDisplay.size > 1) {
+            // choosing random tile from the display
+            currentGame.offerDisplay.random()
+        } else if (Random.nextBoolean()) { // random decision when only one tile left
+            currentGame.offerDisplay.random()
+        }
+        else {
+            currentGame.stacks[0]
+        }
+
+        // placing process
+        rootService.playerActionService.placeTile(randomTile, randomDirection, randomLocation)
+    }
+}
