670b495711df426edb3242c5c11bb727a22b0f3c
Karina <karina93209@gmail.com>
2023-06-30 22:17:40 +0200

    test and KDoc for random AI

--- src/test/kotlin/service/KIServiceRandomTest.kt
+++ src/test/kotlin/service/KIServiceRandomTest.kt
@@ -2,19 +2,31 @@ package service
 
 import entity.*
 import kotlin.test.*
+
+/**
+ * [KIServiceRandomTest] tests [KIServiceRandom]
+ */
 class KIServiceRandomTest {
 
+    //initialize data for tests
     val rootService = RootService()
     private val john = Triple("JohnAI", Color.BLACK, PlayerType.RANDOM_AI)
     private val jo = Triple("JoAI", Color.WHITE, PlayerType.RANDOM_AI)
     private val playerNames = mutableListOf(john, jo)
 
+    /**
+     * [calculateRandomMoveTest] tests [KIServiceRandom.calculateRandomMove].
+     * General [PlayerActionService.placeTile], which was used in function, was already tested in [PlaceTileTest].
+     * All test were proved manually because AI is random
+     */
     @Test
     fun calculateRandomMoveTest() {
-        var tile1 = Tile(1, 6, Element.WATER, listOf(
-            Arrow(Element.FIRE, Direction.UP),
-            Arrow(Element.AIR, Direction.DOWN),
-            Arrow(Element.EARTH, Direction.RIGHT)
+
+        // phase with one element in offer display
+        val tile1 = Tile(4, 6, Element.FIRE, listOf(
+            Arrow(Element.WATER, Direction.UP),
+            Arrow(Element.AIR, Direction.UP_RIGHT),
+            Arrow(Element.EARTH, Direction.UP_LEFT)
         ))
 
         rootService.gameService.startNewGame(playerNames)
@@ -22,6 +34,7 @@ class KIServiceRandomTest {
         assertNotNull(game)
 
         game.players[0].discs.clear()
+
         repeat(24) {
             game.players[0].discs.add(Disc.SOUND)
         }
@@ -30,18 +43,51 @@ class KIServiceRandomTest {
 
         game.offerDisplay.add(tile1)
 
+        // on the board will be placed either tile from offer display or first card from stack
+        // with the same probability
+        println("offer display with one tile" + game.offerDisplay)
+        println("first element of stack" + game.stacks[0])
+        rootService.kIServiceRandom.calculateRandomMove()
+        // what exactly will be placed on the board
+        println("board with first random tile" + game.players[0].board)
+        rootService.gameService.changeToNextPlayer()
 
-        /*var tile2 = Tile(2, 3, Element.FIRE, listOf(
-            Arrow(Element.EARTH, Direction.DOWN),
-            Arrow(Element.WATER, Direction.UP)
+        // normal phase
+        // creating more elements for offer display
+        val tile2 = Tile(13, 10, Element.FIRE, listOf(
+            Arrow(Element.FIRE, Direction.UP),
+            Arrow(Element.FIRE, Direction.RIGHT),
+            Arrow(Element.AIR, Direction.LEFT),
+            Arrow(Element.AIR, Direction.UP_LEFT)
         ))
 
-        game.offerDisplay.add(tile2)
+        val tile3 = Tile(72, 3, Element.AIR, listOf(
+            Arrow(Element.AIR, Direction.UP_RIGHT),
+            Arrow(Element.FIRE, Direction.UP_LEFT)
+        ))
 
-         */
+        game.offerDisplay.clear()
+        game.offerDisplay.add(tile2)
+        game.offerDisplay.add(tile3)
+
+        println("offer display with two tiles" + game.offerDisplay)
+        // either tile2 or tile3 will be placed
+        rootService.kIServiceRandom.calculateRandomMove()
+        println("players board with two random tiles" + game.players[0].board)
+        rootService.gameService.changeToNextPlayer()
+
+        // intermezzo phase
+        game.intermezzoStorage.clear()
+        game.intermezzoPlayers.addAll(game.players)
+        println("first element of stack" + game.stacks[0])
+        println("second element of stack" + game.stacks[1])
+        game.intermezzoStorage.add(game.stacks[0])
+        game.intermezzoStorage.add(game.stacks[1])
+        println("intermezzo storage" + game.intermezzoStorage)
+        game.intermezzo = true
 
         rootService.kIServiceRandom.calculateRandomMove()
-        println(game.players[0].board)
+        println("intermezzo placement" + game.players[0].board)
         rootService.gameService.changeToNextPlayer()
     }
 }
