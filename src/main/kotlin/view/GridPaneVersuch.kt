089f9747679e4834de1acc74dd6aea0865b74525
marie.quell <marie.quell@tu-dortmund.de>
2023-07-02 12:19:25 +0200

    WIP GridPane

--- src/main/kotlin/view/GridPaneVersuch.kt
+++ src/main/kotlin/view/GridPaneVersuch.kt
@@ -5,117 +5,261 @@ import tools.aqua.bgw.components.ComponentView
 import tools.aqua.bgw.components.container.CardStack
 import tools.aqua.bgw.components.container.LinearLayout
 import tools.aqua.bgw.components.gamecomponentviews.CardView
-
+import tools.aqua.bgw.components.gamecomponentviews.TokenView
 import tools.aqua.bgw.components.layoutviews.*
 import tools.aqua.bgw.components.uicomponents.Label
-import tools.aqua.bgw.core.Alignment
 import tools.aqua.bgw.core.BoardGameScene
 import tools.aqua.bgw.util.Font
 import tools.aqua.bgw.visual.ColorVisual
 
 
 class GridPaneVersuch(private val rootService: RootService) : BoardGameScene(1920, 1080), Refreshable {
-    private val headlineLabel = Label(
-        width = 300, height = 50, posX = 50, posY = 50,
-        text = "GameScene - Comming Soon, Maybe not",
-        font = Font(size = 22)
+
+    /**
+     * Upper Pane
+     */
+    //---------------------------------------------------------
+    /**
+     * defines cardStack
+     */
+    private val cardStack = CardStack<CardView>(
+        width = 120,
+        height = 120,
+        posX = 20,
+        posY = 20,
+        visual = ColorVisual(255, 255, 255, 50)
     )
 
+    //Offer Display
+    private val offer1 = CardStack<CardView>(
+        width = 120,
+        height = 120,
+        posX = 450,
+        posY = 20,
+        visual = ColorVisual(255, 255, 255, 50)
+    )
+    private val offer2 = CardStack<CardView>(
+        width = 120,
+        height = 120,
+        posX = 650,
+        posY = 20,
+        visual = ColorVisual(255, 255, 255, 50)
+    )
+    private val offer3 = CardStack<CardView>(
+        width = 120,
+        height = 120,
+        posX = 850,
+        posY = 20,
+        visual = ColorVisual(255, 255, 255, 50)
+    )
+    private val offer4 = CardStack<CardView>(
+        width = 120,
+        height = 120,
+        posX = 1050,
+        posY = 20,
+        visual = ColorVisual(255, 255, 255, 50)
+    )
+    private val offer5 = CardStack<CardView>(
+        width = 120,
+        height = 120,
+        posX = 1250,
+        posY = 20,
+        visual = ColorVisual(255, 255, 255, 50)
+    )
+
+    //---------------------------------------------------------
+
+
+    /**
+     * Left Pane
+     */
+    //---------------------------------------------------------
+
+    private val smallCardStack1 = CardStack<CardView>(
+        width = 100,
+        height = 100,
+        posX = 20,
+        posY = 20,
+        visual = ColorVisual(255, 255, 255, 50)
+    )
+    private val smallCardStack2 = CardStack<CardView>(
+        width = 100,
+        height = 100,
+        posX = 20,
+        posY = 150,
+        visual = ColorVisual(255, 255, 255, 50)
+    )
+
+    private val soundDiscs = TokenView(
+        width = 50,
+        height = 50,
+        posX = 20,
+        posY = 400,
+        visual = ColorVisual(255, 255, 255)
+    )
+
+    private val cacophonyDiscs = TokenView(
+        width = 50,
+        height = 50,
+        posX = 20,
+        posY = 600,
+        visual = ColorVisual.RED
+    )
+
+    //---------------------------------------------------------
+
+
+    /**
+     * Right Pane
+     */
+    //----------------------------------
+    //Intermezzo
+    private val intermezzoLayout: LinearLayout<CardView> = LinearLayout(
+        width = 100,
+        height = 800,
+        posX = 100,
+        posY = 20,
+        visual = ColorVisual(221, 161, 94)
+    )
+
+    private val intermezzoLabel = Label(
+        width = 200,
+        height = 100,
+        posX = 50,
+        posY = 400,
+        text = "INTERMEZZO",
+        font = Font(size = 30)
+    ).apply {
+        rotation = 90.0
+    }
+
+
+    private val intermezzoOffer1 = CardStack<CardView>(
+        width = 120,
+        height = 120,
+        posX = 10,
+        posY = 70,
+        visual = ColorVisual(GameColor.chaletGreen)
+    )
+    private val intermezzoOffer2 = CardStack<CardView>(
+        width = 120,
+        height = 120,
+        posX = 10,
+        posY = 220,
+        visual = ColorVisual(GameColor.chaletGreen)
+    )
+    private val intermezzoOffer3 = CardStack<CardView>(
+        width = 120,
+        height = 120,
+        posX = 10,
+        posY = 370,
+        visual = ColorVisual(GameColor.chaletGreen)
+    )
+    private val intermezzoOffer4 = CardStack<CardView>(
+        width = 120,
+        height = 120,
+        posX = 10,
+        posY = 520,
+        visual = ColorVisual(GameColor.chaletGreen)
+    )
+    private val intermezzoOffer5 = CardStack<CardView>(
+        width = 120,
+        height = 120,
+        posX = 10,
+        posY = 670,
+        visual = ColorVisual(GameColor.chaletGreen)
+    )
+
+    //-------------------------------------
+
+    /**
+     * Bottom Pane
+     */
+    //-------------------------------------
     private val undoButton = StandardButton(
-        posX = 50, posY = 50,
-        width = 100, height = 50,
+        width = 100,
+        height = 50,
+        posX = 20,
+        posY = 20,
         text = "UNDO"
     )
 
     private val redoButton = StandardButton(
-        posX = 150, posY = 50,
-        width = 100, height = 50,
+        width = 100,
+        height = 50,
+        posX = 150,
+        posY = 20,
         text = "REDO"
     )
 
     private val scoreButton = StandardButton(
-        posX = 350, posY = 980,
-        width = 100, height = 50,
+        width = 100,
+        height = 50,
+        posX = 280,
+        posY = 20,
         text = "SCORE"
     )
 
     private val rotateButton = StandardButton(
-        posX = 500, posY = 980,
-        width = 50, height = 50,
-        text = "rotate"
-    ).apply {
-        componentStyle = "-fx-background-color: #606C38; -fx-background-radius: 50%"
-    }
+        width = 100,
+        height = 50,
+        posX = 410,
+        posY = 20,
+        text = "ROTATE"
+    )
 
     private val confirmButton = StandardButton(
-        posX = 800, posY = 980,
-        width = 50, height = 50,
-        text = "Conf"
-    ).apply {
-        componentStyle = "-fx-background-color: #606C38; -fx-background-radius: 50%"
-    }
-
-    //defines an exact zone where midCards are displayed
-    private val midCardsView: LinearLayout<CardView> = LinearLayout(
-        width = 950,
-        height = 140,
-        posX = 500,
-        posY = 78,
-        spacing = 50,
-        alignment = Alignment.CENTER,
-        visual = ColorVisual(255, 255, 255, 50)
+        width = 100,
+        height = 50,
+        posX = 1780,
+        posY = 20,
+        text = "CONF"
     )
+
+    //-----------------------------------------------------------------------
 
     /**
-     * defines cardStack
+     * Panes
      */
-
-    private val cardStack = CardStack<CardView>(
-        width = 140,
-        height = 140,
-        posX = 160,
-        posY = 160,
-        visual = ColorVisual(255, 255, 255, 50)
-    )
-    private val smallcardStack = CardStack<CardView>(
-        width = 120,
-        height = 120,
-        posX = 160,
-        posY = 320,
-        visual = ColorVisual(255, 255, 255, 50)
-    )
-    private val smallcardStack2 = CardStack<CardView>(
-        width = 120,
-        height = 120,
-        posX = 160,
-        posY = 480,
-        visual = ColorVisual(255, 255, 255, 50)
-    )
-
-
+    //-----------------------------------------------------------------------
     private val targetPane = Pane<ComponentView>(width = 1000, height = 1000)
+    private val cameraPane = CameraPane(0, 0, 756, 134, target = targetPane)
 
 
     private val outerGridPane = GridPane<ComponentView>(0, 0, columns = 1, rows = 3, layoutFromCenter = false)
     private val innerGridPane = GridPane<ComponentView>(columns = 3, rows = 1, layoutFromCenter = false)
 
-    private val upperPane = Pane<ComponentView>(0, 0, 1920, 162, visual = ColorVisual.CYAN)
-    private val bottomPane = Pane<ComponentView>(0, 0, 1920, 162, visual = ColorVisual.MAGENTA)
-    private val leftPane = Pane<ComponentView>(0, 0, 288, 756, visual = ColorVisual.DARK_GRAY)
-    private val rightPane = Pane<ComponentView>(0, 0, 288, 756, visual = ColorVisual.PINK)
+    private val upperPane = Pane<ComponentView>(0, 0, 1920, 162, visual = ColorVisual(GameColor.chaletGreen))
+    private val bottomPane = Pane<ComponentView>(0, 0, 1920, 80, visual = ColorVisual(221, 161, 94))
+    private val leftPane = Pane<ComponentView>(0, 0, 150, 838, visual = ColorVisual(GameColor.chaletGreen))
+    private val rightPane = Pane<ComponentView>(0, 0, 170, 838, visual = ColorVisual(GameColor.chaletGreen))
 
-    //1920, 1080
-    //Pane
+    //-----------------------------------------------------------------------
+
+
     init {
 
         //WIP !
 
+
         initViewStructure()
 
+        val upperPaneList = mutableListOf<ComponentView>(cardStack, offer1, offer2, offer3, offer4, offer5)
 
-        val bottomPaneList = mutableListOf<ComponentView>(redoButton,undoButton,scoreButton,confirmButton,rotateButton)
+        val leftPaneList = mutableListOf<ComponentView>(smallCardStack1, smallCardStack2, cacophonyDiscs, soundDiscs)
+
+        val rightPaneList = mutableListOf<ComponentView>(intermezzoLayout,intermezzoLabel,intermezzoOffer1,intermezzoOffer2,intermezzoOffer3,
+            intermezzoOffer4,intermezzoOffer5)
 
 
+        val bottomPaneList =
+            mutableListOf<ComponentView>(redoButton, undoButton, scoreButton, confirmButton, rotateButton)
+
+
+
+        addComponentsToPane(upperPane, upperPaneList)
+        addComponentsToPane(leftPane, leftPaneList)
+        addComponentsToPane(rightPane, rightPaneList)
         addComponentsToPane(bottomPane, bottomPaneList)
 
 
@@ -126,26 +270,26 @@ class GridPaneVersuch(private val rootService: RootService) : BoardGameScene(192
     /**
      * Properly initializes the rows and columns of the GridPanes and puts the panes together.
      */
-    private fun initViewStructure(){
+    private fun initViewStructure() {
         outerGridPane.setRowHeight(0, 162)
-        outerGridPane.setRowHeight(1, 756)
-        outerGridPane.setRowHeight(2, 162)
+        outerGridPane.setRowHeight(1, 838)
+        outerGridPane.setRowHeight(2, 80)
 
         outerGridPane[0, 0] = upperPane
         outerGridPane[0, 1] = innerGridPane
         outerGridPane[0, 2] = bottomPane
 
-        innerGridPane.setColumnWidth(0, 288)
-        innerGridPane.setColumnWidth(1, 1344)
-        innerGridPane.setColumnWidth(2, 288)
+        innerGridPane.setColumnWidth(0, 150)
+        innerGridPane.setColumnWidth(1, 1600)
+        innerGridPane.setColumnWidth(2, 170)
 
         innerGridPane[0, 0] = leftPane
-        innerGridPane[1, 0] = targetPane
+        innerGridPane[1, 0] = cameraPane
         innerGridPane[2, 0] = rightPane
     }
 
-    private fun addComponentsToPane(pane: Pane<ComponentView>, componentList:MutableList<ComponentView>){
-        for (component in componentList){
+    private fun addComponentsToPane(pane: Pane<ComponentView>, componentList: MutableList<ComponentView>) {
+        for (component in componentList) {
             pane.add(component)
         }
     }
